d96be91f10247780476324200ff78a0906a8321f
CrocoDuck <CrocoDuckoDucks@users.noreply.github.com>
2019-04-07 13:58:23 +0100

    Files generated by PkgTemplates

---
+++ src/OverlapNoise.jl
@@ -0,0 +1,5 @@
+module OverlapNoise
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using OverlapNoise
+using Test
+
+@testset "OverlapNoise.jl" begin
+    # Write your own tests here.
+end
